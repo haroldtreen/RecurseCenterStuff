5a0176e6d9514011fb2a2ba2df9eba0f87e41617
Harold Treen <haroldtreen@gmail.com>
2016-10-31 01:26:26 -0400

    Adds hypemachine bookmarklet.

---
+++ javascript/HypeMachineBookmarklet.js
@@ -0,0 +1,35 @@
+(function() {
+    function getTracks() {
+        var tracks = [];
+        $('.section-player').each(function() {
+            tracks.push({
+                title: $(this).find('.track_name').text().replace(/\s+/g, ' ').trim() + '.mp3',
+                url: $(this).find('.dl').find('a').attr('href'),
+            });
+        });
+        return tracks;
+    }
+
+    function downloadTrack(track) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', track.url, true);
+        xhr.responseType = 'blob';
+        xhr.onload = function(e) {
+            const link = document.createElement('a');
+            link.download = track.title;
+            link.href = (URL || webkitURL).createObjectURL(this.response);
+            link.target = '_blank';
+
+            document.body.appendChild(link);
+            link.click();
+        }
+        xhr.send();
+    }
+
+    const downloadLimit = Number(prompt('How many tracks to download?'));
+    getTracks().forEach(function(track, index) {
+        if (index < downloadLimit) {
+            downloadTrack(track);
+        }
+    });
+}())
---
+++ javascript/README.md
@@ -0,0 +1,6 @@
+# Javscript
+
+### HypeMachineBookmarklet.js
+
+Works with the [HypeMachine Chrome Extension](https://github.com/fzakaria/HypeMachine-Extension) to download multiple mp3s at once.
+
