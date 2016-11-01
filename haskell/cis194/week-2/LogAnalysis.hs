e6912da5f063259ef993cb410ec198899e31687f
Harold Treen <haroldtreen@gmail.com>
2016-11-01 17:33:54 -0400

    Completed Haskell solution for cis194 week 2

--- haskell/cis194/week-2/LogAnalysis.hs
+++ haskell/cis194/week-2/LogAnalysis.hs
@@ -28,3 +28,36 @@ parseMessage _ = Unknown "Unknown Message"
 
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert msg Leaf = Node Leaf msg Leaf
+insert (Unknown _) tree = tree
+insert msg (Node left rootMsg right)
+    | (time msg) < (time rootMsg) = Node (insert msg left) rootMsg right
+    | (time msg) > (time rootMsg) = Node left rootMsg (insert msg right)
+    where
+        time (LogMessage _ tm _) = tm
+        time (Unknown _) = 0
+insert _ tree = tree
+
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build (x:xs) = insert x $ build xs
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left msg right) = (inOrder left) ++ [ msg ] ++ (inOrder right)
+
+interestingError :: LogMessage -> Bool
+interestingError (LogMessage (Error level) _ _)
+    | level > 50 = True
+    | otherwise = False
+interestingError _ = False
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong [] = []
+whatWentWrong (msgs) = map logToString $ inOrder $ build $ filter interestingError msgs
+    where
+        logToString (LogMessage _ _ str) = str
+        logToString (Unknown _) = ""
+
