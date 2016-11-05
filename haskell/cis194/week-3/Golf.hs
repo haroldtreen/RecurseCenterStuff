f0ff1c04c210676845447a44d6ba780c8bbcfab9
Harold Treen <haroldtreen@gmail.com>
2016-11-05 16:43:47 -0400

    Local Maxima solution

--- haskell/cis194/week-3/Golf.hs
+++ haskell/cis194/week-3/Golf.hs
@@ -2,3 +2,9 @@ module Golf where
     skips :: [a] -> [[a]]
     skips [] = []
     skips list = [[list !! (charPos-1) | charPos <- [offset, 2 * offset..length list], mod charPos offset == 0] | offset <- [1..length list]]
+
+    localMaxima :: [Integer] -> [Integer]
+    localMaxima [] = []
+    localMaxima list = [ list !! maxima | maxima <- [1..length list - 1], isMaxima list maxima]
+        where
+            isMaxima l i = l !! (i - 1) < (l !! i) && l !! (i + 1) < (l !! i)
