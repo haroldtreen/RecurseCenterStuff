a0586d24eeb04c29efded7903f93acf2703cd9dc
Harold Treen <haroldtreen@gmail.com>
2016-11-06 14:43:34 -0500

    Adds histogram solution.

--- haskell/cis194/week-3/Golf.hs
+++ haskell/cis194/week-3/Golf.hs
@@ -1,4 +1,6 @@
 module Golf where
+    import qualified Data.Map as M
+
     skips :: [a] -> [[a]]
     skips [] = []
     skips list = [[list !! (charPos-1) | charPos <- [offset, 2 * offset..length list], mod charPos offset == 0] | offset <- [1..length list]]
@@ -8,3 +10,13 @@ module Golf where
     localMaxima list = [ list !! maxima | maxima <- [1..length list - 1], isMaxima list maxima]
         where
             isMaxima l i = l !! (i - 1) < (l !! i) && l !! (i + 1) < (l !! i)
+
+
+    histogram :: [Integer] -> String
+    histogram nums = unlines [ (getRow countsMap rowNum) | rowNum <- [histogramMax, histogramMax - 1..1] ] ++ unlines [replicate 10 '=', "0123456789"]
+        where
+            countsMap = foldl (\m v -> M.insertWith (+) v 1 m) M.empty nums
+            histogramMax = maximum $ M.elems countsMap
+
+    getRow :: M.Map Integer Integer -> Integer -> String
+    getRow counts rowNum = foldl (\str col -> if (M.findWithDefault 0 col counts >= rowNum) then do str ++ "*" else do str ++ " ") "" [0..9]
