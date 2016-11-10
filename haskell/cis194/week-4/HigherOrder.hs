2336537b3450867e8c3ffb62f5a1d2d9e85bc075
Harold Treen <haroldtreen@gmail.com>
2016-11-10 17:01:45 -0500

    Rest of week 4 homework.

--- haskell/cis194/week-4/HigherOrder.hs
+++ haskell/cis194/week-4/HigherOrder.hs
@@ -1,5 +1,5 @@
 module HigerOrder where
-    {-Exercise 1-}
+    -- Exercise 1
 
     fun1 :: [Integer] -> Integer
     fun1 [] = 1
@@ -21,7 +21,7 @@ module HigerOrder where
         . takeWhile (1 /=)
         . iterate (\x -> if even x then do (x `div` 2) else do (3 * x + 1))
 
-    {-Exercise 2-}
+    -- Exercise 2
 
     data Tree a = Leaf
         | Node Integer (Tree a) a (Tree a)
@@ -31,7 +31,6 @@ module HigerOrder where
     foldTree :: [a] -> Tree a
     foldTree [] = Leaf
     foldTree x = foldr (\v t -> insertBalanced t v) Leaf x
-
 
     insertBalanced :: Tree a -> a -> Tree a
     insertBalanced Leaf el = Node 1 Leaf el Leaf
@@ -49,3 +48,23 @@ module HigerOrder where
     getHeight :: Tree a -> Integer
     getHeight Leaf = 0
     getHeight (Node _ left _ right) = 1 + (maximum [getHeight left, getHeight right])
+
+    -- Excercise 3
+
+    xor :: [Bool] -> Bool
+    xor = (foldr (\curr cumm -> ((not cumm) && curr) || (cumm && (not curr))) False)
+
+    map' :: (a -> b) -> [a] -> [b]
+    map' trans arr = foldr (\curr cumm -> (trans curr) : cumm) [] arr
+
+    -- Excercise 4
+
+    sieveSundaram :: Integer -> [Integer]
+    sieveSundaram n = [ 2 * i + 1 | i <- [1..n], not (i `elem` exclude)]
+        where
+            exclude = sieveExclude n
+
+    sieveExclude :: Integer -> [Integer]
+    sieveExclude n = filter (<= n) allNumbers
+        where
+            allNumbers = [ j + i + 2 * j * i | j <- [1..n], i <- [j..n] ]
