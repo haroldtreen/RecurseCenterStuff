518a5c64328915c7e1e3ee2567e89435486d5eb2
Harold Treen <haroldtreen@gmail.com>
2016-12-01 02:30:34 -0500

    Add advent of code day 1 solution

---
+++ advent-of-code/README.md
@@ -0,0 +1,5 @@
+# Advent of Code
+
+> Advent of Code solutions
+
+See problems here: http://adventofcode.com/
---
+++ advent-of-code/day-1.js
@@ -0,0 +1,52 @@
+let currentDir = 0;
+let totalSteps = { N: 0, E: 0, S: 0, W: 0, };
+let pastDeltas = {};
+
+const turn = (dir) => {
+    if (dir === 'L') {
+        currentDir = (currentDir - 1) < 0 ? 3 : currentDir - 1;
+    } else if (dir === 'R') {
+        currentDir = (currentDir + 1) > 3 ? 0 : currentDir + 1;
+    }
+};
+
+const delta = () => {
+    const horizontalSteps = Math.abs(totalSteps.E - totalSteps.W);
+    const horizontalDir = (totalSteps.E - totalSteps.W) >= 0 ? 'E' : 'W';
+    const verticalSteps = Math.abs(totalSteps.N - totalSteps.S);
+    const verticalDir = (totalSteps.N - totalSteps.S) >= 0 ? 'N' : 'S';
+
+    return horizontalSteps + horizontalDir + ' ' + verticalSteps + verticalDir;
+};
+
+const walk = (steps) => {
+    const compass = ['N', 'E', 'S', 'W'];
+    const direction = compass[currentDir];
+    const deltas = [];
+
+    for (var i = 0; i < steps; i++) {
+        totalSteps[direction] += 1;
+        deltas.unshift(delta());
+    }
+    return deltas;
+};
+
+const inputsStr = 'R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5, R2, R2, R1, L2, L3, L2, L1, R3, L5, R187, R1, R4, L1, R5, L3, L4, R50, L4, R2, R70, L3, L2, R4, R3, R194, L3, L4, L4, L3, L4, R4, R5, L1, L5, L4, R1, L2, R4, L5, L3, R4, L5, L5, R5, R3, R5, L2, L4, R4, L1, R3, R1, L1, L2, R2, R2, L3, R3, R2, R5, R2, R5, L3, R2, L5, R1, R2, R2, L4, L5, L1, L4, R4, R3, R1, R2, L1, L2, R4, R5, L2, R3, L4, L5, L5, L4, R4, L2, R1, R1, L2, L3, L2, R2, L4, R3, R2, L1, L3, L2, L4, L4, R2, L3, L3, R2, L4, L3, R4, R3, L2, L1, L4, R4, R2, L4, L4, L5, L1, R2, L5, L2, L3, R2, L2';
+const inputs = inputsStr.split(', ').map((move) => {
+    const matches = move.match(/(\w)(\d+)/);
+    return { dir: matches[1], steps: Number(matches[2]) };
+});
+
+inputs.forEach((input) => {
+    turn(input.dir);
+    const deltas = walk(input.steps);
+    deltas.forEach((d) => {
+        if (pastDeltas[d]) {
+            console.log('Visited twice: ' + d);
+        } else {
+            pastDeltas[d] = true;
+        }
+    });
+});
+
+console.log('Final Destinateion: ' + delta());
